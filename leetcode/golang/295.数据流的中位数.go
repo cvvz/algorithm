94d7549cc5146aac99fa26023f369f2da329b66b
weizhi <ftdchenwz@gmail.com>
2021-10-10 16:15:38 +0800

    heap

--- leetcode/golang/295.数据流的中位数.go
+++ leetcode/golang/295.数据流的中位数.go
@@ -5,122 +5,70 @@
  */
 
 // @lc code=start
-
-/****** 不使用库函数，底层实现细节和库函数是一样的 *******/
-
 type MedianFinder struct {
-	minHeap MinHeap
-	maxHeap MaxHeap
-}
-
-type Interface interface {
-	Less(int, int) bool
-	Swap(int, int)
-	Len() int
-}
-
-type MinHeap []int
-type MaxHeap []int
-
-func (mh MinHeap) Less(i, j int) bool {
-	return mh[i] < mh[j]
-}
-
-func (mh MaxHeap) Less(i, j int) bool {
-	return mh[i] > mh[j]
-}
-
-func (mh MinHeap) Swap(i, j int) {
-	mh[i], mh[j] = mh[j], mh[i]
-}
-
-func (mh MaxHeap) Swap(i, j int) {
-	mh[i], mh[j] = mh[j], mh[i]
-}
-
-func (mh MinHeap) Len() int {
-	return len(mh)
-}
-
-func (mh MaxHeap) Len() int {
-	return len(mh)
-}
-
-func HeapifyUp(h Interface, i int) {
-	for {
-		parent := (i - 1) / 2
-		if i == parent || !h.Less(i, parent) {
-			break
-		}
-		h.Swap(i, parent)
-		i = parent
-	}
-}
-
-func HeapifyDown(h Interface, i int) {
-	parent := i
-	l := i*2 + 1
-	r := i*2 + 2
-
-	if l < h.Len() && h.Less(l, parent) {
-		parent = l
-	}
-	if r < h.Len() && h.Less(r, parent) {
-		parent = r
-	}
-	if parent != i {
-		h.Swap(i, parent)
-		HeapifyDown(h, parent)
-	}
+	maxHeap *MaxIntHeap
+	minHeap *IntHeap
 }
 
 /** initialize your data structure here. */
 func Constructor() MedianFinder {
 	return MedianFinder{
-		minHeap: MinHeap{},
-		maxHeap: make(MaxHeap, 0),
+		maxHeap: &MaxIntHeap{},
+		minHeap: &IntHeap{},
 	}
 }
 
+// 小顶堆
+type IntHeap struct {
+	sort.IntSlice
+}
+
+func (h *IntHeap) Push(x interface{}) {
+	h.IntSlice = append(h.IntSlice, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	v := h.IntSlice[h.IntSlice.Len()-1]
+	h.IntSlice = h.IntSlice[:h.IntSlice.Len()-1]
+	return v
+}
+
+func (h *IntHeap) Front() int {
+	return h.IntSlice[0]
+}
+
+// 大顶堆, 官方答案用的是取负来实现大顶堆的效果
+type MaxIntHeap struct {
+	IntHeap
+}
+
+func (h *MaxIntHeap) Less(i, j int) bool {
+	return h.IntHeap.IntSlice.Less(j, i)
+}
+
 func (this *MedianFinder) AddNum(num int) {
-	if len(this.maxHeap) == 0 {
-		this.maxHeap = append(this.maxHeap, num)
-		return
-	}
-	if num <= this.maxHeap[0] {
-		if len(this.maxHeap) > len(this.minHeap) {
-			this.minHeap = append(this.minHeap, this.maxHeap[0])
-			HeapifyUp(this.minHeap, len(this.minHeap)-1)
-			this.maxHeap[0] = num
-			HeapifyDown(this.maxHeap, 0)
-		} else {
-			this.maxHeap = append(this.maxHeap, num)
-			HeapifyUp(this.maxHeap, len(this.maxHeap)-1)
+	if this.maxHeap.Len() == 0 || num < this.maxHeap.Front() {
+		heap.Push(this.maxHeap, num)
+		if this.maxHeap.Len() > this.minHeap.Len()+1 {
+			heap.Push(this.minHeap, heap.Pop(this.maxHeap))
 		}
 	} else {
-		if len(this.maxHeap) > len(this.minHeap) {
-			this.minHeap = append(this.minHeap, num)
-			HeapifyUp(this.minHeap, len(this.minHeap)-1)
-		} else {
-			this.minHeap = append(this.minHeap, num)
-			HeapifyUp(this.minHeap, len(this.minHeap)-1)
-
-			this.maxHeap = append(this.maxHeap, this.minHeap[0])
-			HeapifyUp(this.maxHeap, len(this.maxHeap)-1)
-
-			this.minHeap[0] = this.minHeap[len(this.minHeap)-1]
-			this.minHeap = this.minHeap[:len(this.minHeap)-1]
-			HeapifyDown(this.minHeap, 0)
+		heap.Push(this.minHeap, num)
+		if this.minHeap.Len() > this.maxHeap.Len() {
+			heap.Push(this.maxHeap, heap.Pop(this.minHeap))
 		}
 	}
 }
 
 func (this *MedianFinder) FindMedian() float64 {
-	if len(this.maxHeap) > len(this.minHeap) {
-		return float64(this.maxHeap[0])
+	if this.maxHeap.Len() > this.minHeap.Len() {
+		return float64(this.maxHeap.Front())
+	} else {
+		return float64(this.maxHeap.Front()+this.minHeap.Front()) / 2.0
 	}
-	return float64((this.minHeap[0] + this.maxHeap[0])) / 2.0
 }
+
+// 注意这里大顶堆的实现, 官方答案用的是取负来实现大顶堆的效果
 
 /**
  * Your MedianFinder object will be instantiated and called as such:
