18db11fe5a58f5d700a85a813a44b99dd438b366
weizhi <ftdchenwz@gmail.com>
2021-12-26 09:12:14 +0800

    inorder

--- leetcode/golang/94.二叉树的中序遍历.go
+++ leetcode/golang/94.二叉树的中序遍历.go
@@ -14,7 +14,51 @@
  * }
  */
 func inorderTraversal(root *TreeNode) []int {
+	var ans []int
+	var s stack
 
+	if root == nil {
+		return ans
+	}
+
+	s.push(root)
+
+	for !s.empty() {
+		node := s.top()
+		if node.Left != nil {
+			s.push(node.Left)
+			node.Left = nil
+		} else {
+			_ = s.pop()
+			ans = append(ans, node.Val)
+			if node.Right != nil {
+				s.push(node.Right)
+				node.Right = nil
+			}
+		}
+	}
+
+	return ans
+}
+
+type stack []*TreeNode
+
+func (s *stack) push(node *TreeNode) {
+	*s = append(*s, node)
+}
+
+func (s *stack) pop() *TreeNode {
+	top := s.top()
+	*s = (*s)[:len(*s)-1]
+	return top
+}
+
+func (s *stack) top() *TreeNode {
+	return (*s)[len(*s)-1]
+}
+
+func (s *stack) empty() bool {
+	return len(*s) == 0
 }
 
 //🌟 二叉查找树用中序遍历能得到顺序数组
